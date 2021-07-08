422bee9ee9aa1a83a21ebaa20e8d5d71d471586d
Guilherme <guilherme_devz@hotmail.com>
2021-07-08 16:42:53 -0300

    UPDATE: adding the styles for when the total balance is negative

--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -30,5 +30,9 @@ export const Container = styled.div`
       background: var(--green);
       color: var(--shape);
     }
+
+    &.highlight-background.red {
+      background: var(--red);
+    }
   }
 `;
