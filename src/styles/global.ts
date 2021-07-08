deec5615b6179c25c252d5b8936e496aaffb21f8
Guilherme <guilherme_devz@hotmail.com>
2021-07-08 16:46:39 -0300

    UPDATE: changing the color of var '--background'

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   :root{
-    --background: #E5E5E5;
+    --background: #F0F2F5;
     --red: #e52e40;
     --blue: #5429cc;
     --green: #33CC95;
@@ -54,5 +54,30 @@ export const GlobalStyle = createGlobalStyle`
   [disabled]{
     opacity: 0.6;
     cursor: not-allowed;
+  }
+
+  .react-modal-overlay{
+    background: rgba(0,0,0,0.5);
+
+    position: fixed;
+    top:0;
+    bottom:0;
+    right: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content:center;
+  }
+
+  .react-modal-content{
+    max-width:580px;
+    width: 100%;
+
+    background: var(--background);
+    padding:3rem;
+
+    position: relative;
+    border-radius: 0.25rem;
   }
 `;
